0aefaf689e8f3bd8d49dcf446445755296bf6fbb
NoboniSultan <sultannoboni@gmail.com>
2025-10-21 14:38:56 -0400

    PureVsImpure

--- 1-pure-impure.js
+++ 1-pure-impure.js
@@ -1,9 +1,20 @@
 const nums = [1, 2, 3]
 
 const doublePure = (arr) => {
-  
+    const newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        newArr.push(arr[i] * 2);
+
+    }
+    return newArr;
 };
 
 const doubleImpure = (arr) => {
-  
+    for (let i = 0; i < arr.length; i++) {
+        arr[i] = arr[i] * 2;
+    }
+    return arr;
 };
+console.log(doubleImpure(nums));
+
+console.log(doublePure(nums));
